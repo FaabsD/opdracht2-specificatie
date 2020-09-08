925c57d262993ae59357b7cd6d24cd10fb7e6530
Fabian <19959@ma-web.nl>
2020-09-08 11:10:09 +0200

    kleine aanpassing

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 let studentNummer = 19959;
 let klas = "MD2Ab";
 
-const student = {
+let student = {
     studentNummer: studentNummer,
     klas: klas
 };
-console.log(student);
+console.table(student);
