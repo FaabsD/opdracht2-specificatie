dfbcda24144d20a4dc049a5248d98ab810006341
Fabian Hendriks <19959@ma-web.nl>
2020-09-15 10:42:46 +0200

    Script wat uitgebreidt

--- script.js
+++ script.js
@@ -1,8 +1,28 @@
-let studentNummer = 19959;
-let klas = "MD2Ab";
+function Student(studentNummer, klas){
+    let studentNummer = studentNummer;
+    let klas = klas;
+}
 
-let student = {
-    studentNummer: studentNummer,
-    klas: klas
-};
-console.table(student);
+let groepje = [];
+
+let Mike = new Student(30079, 'MD2Aa');
+let Bob = new Student(29198, 'MD2Aa');
+let Firas = new Student(29003, 'Md2Aa');
+let Justin = new Student(28454, 'MD2Aa');
+let Ruben = new Student(28644, 'MD2Aa');
+let Fabian = new Student(19959, 'MD2Ab');
+
+let compleetGroepje = groepje.push(Mike, Bob, Firas, Justin, Ruben, Fabian );
+
+
+/*
+Mike Yang
+
+Bob van Berckel
+
+Firas Soekhai
+
+Justin Rodriquez Montoya
+
+Ruben Vuurman
+ */
