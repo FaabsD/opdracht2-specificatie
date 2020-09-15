b6d5131a59158faa6003ed81ea6a3067c0996c2b
Fabian Hendriks <19959@ma-web.nl>
2020-09-15 09:51:07 +0200

    Units in een aparte JavaScript file gezet en tevens eigen method toegepast voor opdracht 3 uit de Units opdracht

---
+++ units.js
@@ -0,0 +1,40 @@
+function Voorraad(item, aantal) {
+    let artikel = item;
+    let voorraad = aantal;
+
+    this.getVoorraad = function () {
+        return voorraad;
+    }
+    this.getArtikel = function () {
+        return artikel;
+    }
+    this.showVoorraad = function () {
+        console.log('De voorraad van : ' + artikel + ' is ' + voorraad);
+    }
+    this.verlaagVoorraad = function () {
+        voorraad = voorraad  -1;
+    }
+     // Voor opdracht 3
+    this.verhoogVoorraad = function () {
+        voorraad = voorraad +1;
+    }
+}
+
+function Auto(merk, bouwjaar) {
+
+    let brand = merk;
+    let year = bouwjaar;
+
+    this.getBrand = function () {
+        return brand;
+    }
+
+    this.getYear = function () {
+        return year;
+    }
+
+    this.showDetails = function () {
+        console.log('Automerk: ' + brand + ‘ bouwjaar: ' + bouwjaar);
+    }
+
+}
