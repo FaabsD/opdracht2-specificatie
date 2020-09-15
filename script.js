562c6218f24d171f55771f0e4a2baa334a0684ce
Fabian Hendriks <19959@ma-web.nl>
2020-09-15 10:47:30 +0200

    Update script.js

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ function Student(studentNummer, klas){
     let klas = klas;
 }
 
-let groepje = [];
+let alleStudenten = [];
 
 let Mike = new Student(30079, 'MD2Aa');
 let Bob = new Student(29198, 'MD2Aa');
@@ -12,7 +12,12 @@ let Justin = new Student(28454, 'MD2Aa');
 let Ruben = new Student(28644, 'MD2Aa');
 let Fabian = new Student(19959, 'MD2Ab');
 
-let compleetGroepje = groepje.push(Mike, Bob, Firas, Justin, Ruben, Fabian );
+let studentenCompleet = alleStudenten.push(Mike, Bob, Firas, Justin, Ruben, Fabian );
+
+localStorage.setItem('studentenCompleet', studentenCompleet);
+const studenten = localStorage.getItem(studentenCompleet);
+
+console.log(studenten);
 
 
 /*
